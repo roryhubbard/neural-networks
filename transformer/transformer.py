50c8802a583533b64856a0aea36e4e769a02de81
Rory Hubbard <calvin3h@gmail.com>
2021-11-19 16:38:40 +0100

    proper data preprocessing

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -1,10 +1,12 @@
 import copy
+from tqdm import tqdm
 import numpy as np
 import spacy
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader
 
 import torchtext
@@ -13,17 +15,8 @@ from torchtext.data.utils import get_tokenizer
 from torchtext.datasets import IWSLT2017
 
 
-# ref: https://nlp.seas.harvard.edu/2018/04/03/attention.html
-
-
 def clones(module, N):
   return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
-
-
-def subsequent_mask(size):
-  attn_shape = (1, size, size)
-  subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
-  return torch.from_numpy(subsequent_mask) == 0
 
 
 def scaled_dot_product_attention(Q, K, V, dk):
@@ -33,18 +26,18 @@ def scaled_dot_product_attention(Q, K, V, dk):
 
 class Transformer(nn.Module):
 
-  def __init__(self, d_model, d_ff, h, vocab, dropout=0.1):
+  def __init__(self, d_model, d_ff, h, src_vocab, tgt_vocab, dropout):
     """
     d_model = dimension of layers between modules
     d_ff = dimension of hidden layer in feed forward sublayer
     h = number of heads in mulit-head-attention
     """
     super().__init__()
-    self.embeddings = Embeddings(vocab, d_model)
+    self.embeddings = Embeddings(src_vocab, d_model)
     self.positional_encoder = PositionalEncoding(d_model, dropout)
-    self.encoder = Encoder(d_model, d_ff, h)
-    self.decoder = Decoder(d_model, d_ff, h)
-    self.generator(d_model, vocab)
+    self.encoder = Encoder(d_model, d_ff, h, dropout)
+    self.decoder = Decoder(d_model, d_ff, h, dropout)
+    self.generator = Generator(d_model, tgt_vocab)
 
   def forward(self, x):
     h = self.embeddings(x)
@@ -66,9 +59,9 @@ class Generator(nn.Module):
 
 class Encoder(nn.Module):
 
-  def __init__(self, d_model, d_ff, h):
+  def __init__(self, d_model, d_ff, h, dropout):
     super().__init__()
-    self.layers = clones(EncoderLayer(d_model, d_ff, h), 6)
+    self.layers = clones(EncoderLayer(d_model, d_ff, h, dropout), 6)
 
   def forward(self, x, mask):
     for l in self.layers:
@@ -78,11 +71,11 @@ class Encoder(nn.Module):
 
 class EncoderLayer(nn.Module):
 
-  def __init__(self, d_model, d_ff, h):
+  def __init__(self, d_model, d_ff, h, dropout):
     super().__init__()
-    self.self_attention = MultiHeadAttention(d_model, h)
+    self.self_attention = MultiHeadAttention(d_model, h, dropout)
     self.norm1 = LayerNorm(d_model)
-    self.ff = PositionwiseFeedForward(d_model, d_ff)
+    self.ff = PositionwiseFeedForward(d_model, d_ff, dropout)
     self.norm2 = LayerNorm(d_model)
 
   def forward(self, x, mask):
@@ -95,13 +88,25 @@ class EncoderLayer(nn.Module):
 
 class Decoder(nn.Module):
 
-  def __init__(self, d_model, d_ff, h):
+  def __init__(self, d_model, d_ff, h, dropout):
     super().__init__()
-    self.self_attention = MultiHeadAttention(d_model, h)
+    self.layers = clones(DecoderLayer(d_model, d_ff, h, dropout), 6)
+
+  def forward(self, x, mask):
+    for l in self.layers:
+      h = l(x, mask)
+    return h
+
+
+class DecoderLayer(nn.Module):
+
+  def __init__(self, d_model, d_ff, h, dropout):
+    super().__init__()
+    self.self_attention = MultiHeadAttention(d_model, h, dropout)
     self.norm1 = LayerNorm(d_model)
-    self.source_attention = MultiHeadAttention(d_model, h)
+    self.source_attention = MultiHeadAttention(d_model, h, dropout)
     self.norm2 = LayerNorm(d_model)
-    self.ff = PositionwiseFeedForward(d_model, d_ff)
+    self.ff = PositionwiseFeedForward(d_model, d_ff, dropout)
     self.norm3 = LayerNorm(d_model)
 
   def forward(self, s, x, self_mask, source_mask):
@@ -116,12 +121,13 @@ class Decoder(nn.Module):
 
 class MultiHeadAttention(nn.Module):
 
-  def __init__(self, d_model, h):
+  def __init__(self, d_model, h, dropout):
     super().__init__()
     assert d_model % h == 0
     dk = d_model // h
     self.WO = nn.Parameter(torch.empty((d_model, d_model)))
     self.layers = clones(SingleHeadAttention(d_model, dk), h)
+    self.dropout = nn.Dropout(dropout)
     self.reset_weights()
 
   def reset_weights(self):
@@ -129,7 +135,8 @@ class MultiHeadAttention(nn.Module):
 
   def forward(self, Q, K, V):
     monolith_head = torch.cat([l(Q, K, V) for l in self.layers], dim=-1)
-    return monolith_head @ self.WO
+    h = monolith_head @ self.WO
+    return self.dropout(h)
 
 
 class SingleHeadAttention(nn.Module):
@@ -167,7 +174,10 @@ class PositionwiseFeedForward(nn.Module):
     self.dropout = nn.Dropout(dropout)
 
   def forward(self, x):
-    return self.h2(self.dropout(F.relu(self.h1(x))))
+    h = self.h1(x)
+    h = F.relu(h)
+    h = self.h2(h)
+    return self.dropout(h)
 
 
 class LayerNorm(nn.Module):
@@ -209,55 +219,77 @@ class PositionalEncoding(nn.Module):
     self.register_buffer('pe', pe)
 
   def forward(self, x):
-    x = x + Variable(self.pe[:, :x.size(1)], 
-                     requires_grad=False)
+    x = x + Variable(self.pe[:, :x.size(1)], requires_grad=False)
     return self.dropout(x)
 
 
-def data_process(text_pairs, en_tokenizer, nl_tokenizer, en_vocab, nl_vocab):
-  data = []
-  for en_text, nl_text in text_pairs:
-    en_tensor = torch.tensor(en_vocab(en_tokenizer(en_text)), dtype=torch.long)
-    nl_tensor = torch.tensor(nl_vocab(en_tokenizer(nl_text)), dtype=torch.long)
-    data.append((en_tensor, nl_tensor))
-  return data
+def additive_subsequent_mask(sz):
+  mask = torch.triu(torch.ones((sz, sz)).type(torch.bool), diagonal=1)
+  return torch.zeros((sz, sz)).masked_fill(mask, float('-inf'))
+
+
+def create_mask(src, tgt):
+  src_seq_len = src.shape[0]
+  tgt_seq_len = tgt.shape[0]
+
+  tgt_mask = additive_subsequent_mask(tgt_seq_len)
+  src_mask = torch.zeros((src_seq_len, src_seq_len)).type(torch.bool)
+
+  src_padding_mask = (src == PAD_IDX).transpose(0, 1)
+  tgt_padding_mask = (tgt == PAD_IDX).transpose(0, 1)
+  return src_mask, tgt_mask, src_padding_mask, tgt_padding_mask
+
+
+def text_to_tensor(text, vocab, tokenizer):
+  return torch.cat((torch.tensor([vocab['<bos>']]),
+                   torch.tensor(vocab(tokenizer(text))),
+                   torch.tensor([vocab['<eos>']])))
 
 
 def main():
-  en_nlp = spacy.load('en_core_web_sm')
-  nl_nlp = spacy.load('nl_core_news_sm')
+  src_tokenizer = get_tokenizer('spacy', language='en_core_web_sm')
+  tgt_tokenizer = get_tokenizer('spacy', language='nl_core_news_sm')
 
-  en_tokenizer = get_tokenizer('spacy', language='en_core_web_sm')
-  nl_tokenizer = get_tokenizer('spacy', language='nl_core_news_sm')
+  train_iter = IWSLT2017(root='/home/chubb/datasets',
+                         split='train', language_pair=('en', 'nl'))
+
+  src_train, tgt_train = list(zip(*list(train_iter)))
+
+  specials = ['<unk>', '<pad>', '<bos>', '<eos>']
+  src_vocab = build_vocab_from_iterator(map(src_tokenizer, src_train), specials=specials)
+  src_vocab.set_default_index(src_vocab['<unk>'])
+  tgt_vocab = build_vocab_from_iterator(map(tgt_tokenizer, tgt_train), specials=specials)
+  tgt_vocab.set_default_index(tgt_vocab['<unk>'])
+
+  def collate_fn(batch):
+    src_batch, tgt_batch = [], []
+    for src_sample, tgt_sample in batch:
+      src_batch.append(text_to_tensor(src_sample.rstrip('\n'), src_vocab, src_tokenizer))
+      tgt_batch.append(text_to_tensor(tgt_sample.rstrip('\n'), tgt_vocab, tgt_tokenizer))
+
+    src_batch = pad_sequence(src_batch, padding_value=src_vocab['<pad>'])
+    tgt_batch = pad_sequence(tgt_batch, padding_value=tgt_vocab['<pad>'])
+    return src_batch, tgt_batch
 
   train_iter, val_iter, test_iter = IWSLT2017(root='/home/chubb/datasets',
                                               language_pair=('en', 'nl'))
 
-  en_train, nl_train = list(zip(*list(train_iter)))
-
-  en_vocab = build_vocab_from_iterator(map(en_tokenizer, en_train),
-                                       specials=['<unk>', '<pad>', '<bos>', '<eos>'])
-  en_vocab.set_default_index(en_vocab['<unk>'])
-  nl_vocab = build_vocab_from_iterator(map(nl_tokenizer, nl_train),
-                                       specials=['<unk>', '<pad>', '<bos>', '<eos>'])
-  nl_vocab.set_default_index(nl_vocab['<unk>'])
-
-  train_data = data_process(train_iter, en_tokenizer, nl_tokenizer, en_vocab, nl_vocab)
-  val_data = data_process(val_iter, en_tokenizer, nl_tokenizer, en_vocab, nl_vocab)
-  test_data = data_process(test_iter, en_tokenizer, nl_tokenizer, en_vocab, nl_vocab)
-
   batch_size = 64
-
-  train_iter = DataLoader(train_data, batch_size=batch_size)
-  val_iter = DataLoader(val_data, batch_size=batch_size)
-  test_iter = DataLoader(test_data, batch_size=batch_size)
+  train_loader = DataLoader(train_iter, batch_size=batch_size, collate_fn=collate_fn)
+  val_loader = DataLoader(val_iter, batch_size=batch_size, collate_fn=collate_fn)
+  test_loader = DataLoader(test_iter, batch_size=batch_size, collate_fn=collate_fn)
 
   d_model = 512
   d_ff = 2048
   h = 8
-  vocab = max(len(en_vocab), len(nl_vocab))
+  dropout = 0.1
 
-  transformer = Transformer(d_model, d_ff, h, vocab)
+  transformer = Transformer(d_model, d_ff, h, len(src_vocab), len(tgt_vocab), dropout)
+
+  for src_batch, tgt_batch in tqdm(train_loader):
+    print(src_batch.shape)
+    print(tgt_batch.shape)
+    return
 
 
 if __name__ == "__main__":
